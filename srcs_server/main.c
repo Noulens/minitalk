0a9215d74c132faa3aeffedd6ef4cea7f13a0d72
Thibault NOULENS <tnoulens@e2r3p13.clusters.42paris.fr>
2022-08-01 17:25:43 +0200

    tno 01/08 17.25

--- srcs_server/main.c
+++ srcs_server/main.c
@@ -6,7 +6,7 @@
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/01 14:50:17 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/08/01 16:15:57 by tnoulens         ###   ########.fr       */
+/*   Updated: 2022/08/01 17:25:06 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 int	main(int argc, char **argv)
 {
-	pid_t the_pid;
+	pid_t	the_pid;
 
 	(void)argv;
 	if (argc != 1)
 		return (1);
 	the_pid = getpid();
-	printf(GREEN"Server ready, PID: %d\n"END, the_pid);
+	ft_printf(GREEN"Server ready, PID: %d\n"END, the_pid);
+	while (1)
+		pause();
 }
