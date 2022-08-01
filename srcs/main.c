b75f630bf0e8f82b855245a4d7ce462d7824ad22
Thibault NOULENS <tnoulens@e2r3p13.clusters.42paris.fr>
2022-08-01 14:59:36 +0200

    tno 01/08 14.59

--- srcs/main.c
+++ srcs/main.c
@@ -1,24 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minitalk.h                                         :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/01 13:51:46 by tnoulens          #+#    #+#             */
-/*   Updated: 2022/08/01 14:59:12 by tnoulens         ###   ########.fr       */
+/*   Created: 2022/08/01 14:50:17 by tnoulens          #+#    #+#             */
+/*   Updated: 2022/08/01 14:50:19 by tnoulens         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINITALK_H
-# define MINITALK_H
-
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-# include <signal.h>
-
-
-/* Proto */
-
-#endif
